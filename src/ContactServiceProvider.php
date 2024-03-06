929a91c9dc7953a1b7ee2ef83709bd9bf4132a4a
akramghaleb <46090581+akramghaleb@users.noreply.github.com>
2024-03-06 11:43:13 +0300

    Add tag to publish

--- src/ContactServiceProvider.php
+++ src/ContactServiceProvider.php
@@ -15,7 +15,7 @@ class ContactServiceProvider extends ServiceProvider{
         );
         $this->publishes([
             __DIR__.'/../config/contact.php' => config_path('contact.php'),
-        ]);
+        ], 'contact-config');
 
     }
 
